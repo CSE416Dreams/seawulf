31992d927ca06ae3a74df48836d07b182d906324
humanbeing000 <saiful.haque@stonybrook.edu>
2022-04-20 14:19:41 -0400

    The ms-seawulf file will be directly used in seawulf. We need to add demographics to them and assigned districts.

---
+++ precinctNeighbors/processSHPFiles.py
@@ -0,0 +1,43 @@
+from csv import DictReader
+import json
+
+csvPath = './shp files/ms_2020/ms_2020.csv'
+adjPath = './shp files/ms_2020/adjList.txt'
+csv = 0
+adj = 0
+seawulfInput = {}
+
+with open(adjPath) as file:
+    adj = file.readlines()
+    file.close()
+
+with open(csvPath, 'r') as file:
+    csv = DictReader(file)
+    i = 0
+    for row in csv:
+        seawulfInput[i + 1] = {}
+        # The following adds the adjacenct Nodes
+        adjText = adj[i].strip()
+        adjNodes = adjText[2:len(adjText) - 1].split(', ')
+        seawulfInput[i + 1]["adjacent_nodes"] = adjNodes
+        candidateVotes = {}
+        for candidate, votes in row.items():
+            if candidate.startswith("G20PRE"):
+                candidateVotes[candidate] = int(votes)
+        population = sum(candidateVotes.values())
+        winningCandidate = max(candidateVotes)
+        votingHistory = ""
+        if winningCandidate == "G20PRERTRU":
+            votingHistory = "R"
+        else:
+            votingHistory = "D"
+        seawulfInput[i + 1]["population"] = population
+        seawulfInput[i + 1]["voting_history"] = votingHistory
+        seawulfInput[i + 1]["dem_votes"] = candidateVotes["G20PREDBID"]
+        seawulfInput[i + 1]["rep_votes"] = candidateVotes["G20PRERTRU"]
+        seawulfInput[i + 1]["district"] = "To be Assigned"
+        i += 1
+    file.close()
+
+with open('./shp files/ms_2020/ms-seawulf.json', 'w') as f:
+    json.dump(seawulfInput, f)
